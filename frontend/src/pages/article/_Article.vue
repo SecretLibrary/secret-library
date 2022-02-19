6e95c42ee3f93710760cba577f4a425b86e54168
eggplantiny <eggplantiny@gmail.com>
2022-02-19 13:44:51 +0900

    [Frontend] Article Page

--- frontend/src/pages/article/_Article.vue
+++ frontend/src/pages/article/_Article.vue
@@ -1,14 +1,27 @@
 <template>
-  Hello {{ articleId }} Article!
+  <div>
+    Hello Article!
+    <div>
+      {{ article }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import { Article } from '@/types/article.type'
 
 const route = useRoute()
 
-const articleId = ref(route.params.articleId)
+const article = ref<Article.Article>()
+
+onMounted(async () => {
+  const articleId = route.params.articleId
+  article.value = await axios.get(`/article/${articleId}`)
+  console.log('fucking', article)
+})
 </script>
 
 <style scoped>
